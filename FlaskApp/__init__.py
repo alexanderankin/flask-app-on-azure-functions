4010b67b8acf45ac839383e10dcdb01d6f17f164
David Ankin <daveankin@gmail.com>
2024-04-01 21:05:19 -0400

    changes

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,24 +1,41 @@
-from flask import Flask
+import time
+
+from flask import Flask, Response
 
 # Always use relative import for custom module
 from .package.module import MODULE_VALUE
 
 app = Flask(__name__)
 
-@app.route("/")
+# @app.route("/")
+# def index():
+#     return (
+#         "Try /hello/Chris for parameterized Flask route.\n"
+#         "Try /module for module import guidance"
+#     )
+
+# @app.route("/hello/<name>", methods=['GET'])
+# def hello(name: str):
+#     return f"hello {name}"
+
+# @app.route("/module")
+# def module():
+#     return f"loaded from FlaskApp.package.module = {MODULE_VALUE}"
+
+
+def generate_numbers():
+    """Generator function to yield numbers 1 to 10 with a 1-second delay."""
+    for number in range(1, 11):
+        yield f"data: {number}\n\n"
+        time.sleep(1)
+
+@app.route('/')
 def index():
-    return (
-        "Try /hello/Chris for parameterized Flask route.\n"
-        "Try /module for module import guidance"
-    )
+    """Serve the SSE stream."""
+    return Response(generate_numbers(), content_type='text/event-stream')
 
-@app.route("/hello/<name>", methods=['GET'])
-def hello(name: str):
-    return f"hello {name}"
 
-@app.route("/module")
-def module():
-    return f"loaded from FlaskApp.package.module = {MODULE_VALUE}"
+
 
 if __name__ == "__main__":
     app.run()
--- HandleApproach/__init__.py
+++ HandleApproach/__init__.py
@@ -4,4 +4,5 @@ from FlaskApp import app
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     """Each request is redirected to the WSGI handler.
     """
+    func.buffer = False
     return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
